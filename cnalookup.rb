fcc11f4984f22b9f16e4b2eb51f21241fe6974ee
schauhan8 <siddharth1989.chauhan@gmail.com>
2013-01-20 02:46:20 -0500

    Yet another bug fix

--- cnalookup.rb
+++ cnalookup.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'mechanize'
 require 'nokogiri'
-require 'hpricot'
 
 FieldNames = {
 		'First Name' => 'ctl00$ContentPlaceHolderMiddleColumn$txtFirstName',
@@ -16,45 +15,39 @@ class URLMaker
 	end
 
 	def InitURL(cnum, fname, lname)
-		puts cnum
-		puts fname
-		puts lname
 		agent = Mechanize.new
 		agent.get(@url)
 		form = agent.page.form_with(:action=>/SearchPage.aspx/)
+		form.radiobuttons.each do |var|
+			puts var.value
+			if var.value == 'rdoTypeNo'
+				form.radiobutton_with(:value => var.value).check
+			end
+		end	
+
 		certNo = form.field_with(:name=>FieldNames['Cert Number'])	
-										
-		form.field_with(:name=>FieldNames['Cert Type']).options[1..-1].each do |var|
-			if(var.value == 'CNA')
+		form.field_with(:name=>FieldNames['Cert Type']).options[2..2].each do |var|
 				form[FieldNames['Cert Type']]='CNA'
-				form[FieldNames['First Name']]=fname #'BENJAMIN P'
-				form[FieldNames['Last Name']]=lname #'CAACON'
-				form[FieldNames['Cert Number']]=cnum#'00561824'
-				form.radiobuttons[1].checked=true
-				#form.radiobuttons[1].checked=true
-				#search_result = form.submit(form.button_with(:value=>'Go'))
+				form[FieldNames['First Name']]='BENJAMIN P'
+				form[FieldNames['Last Name']]='CAACON'
+				form[FieldNames['Cert Number']]='00561824'
+				
+				search_result = form.submit(form.button_with(:value=>'Go'))
 				l = agent.submit(form, form.buttons.first)
+				form["submit"] = 'submit'
 				final = l.body
 				#puts final
-				x = cnum
-				if final.include? cnum
-					puts "??!!"
-				end
-				puts final
-				 
+				re3 = 'No data was found'
 				doc = Nokogiri::HTML(final)
 				doc.search('//td').each do |cell|
 			  		puts cell.content
-			  		if(cell.content).include? 'No data was found that matches your search criteria. Please try again'
-			  			puts 'Not found!'
+			  		if(cell.content).include? re3
+			  			puts re3
 			  			return nil
 					end
 				end
-
 		end
 	end
-end
-
 end
 
 

e74ff731cf6e263590921b0b9c8016c16c8850a9
schauhan8 <siddharth1989.chauhan@gmail.com>
2013-01-19 22:18:10 -0500

    Test1

--- cnalookup.rb
+++ cnalookup.rb
@@ -15,40 +15,41 @@ class URLMaker
 		@url=url
 	end
 
-	def InitURL()
+	def InitURL(cnum, fname, lname)
 		agent = Mechanize.new
 		agent.get(@url)
 		form = agent.page.form_with(:action=>/SearchPage.aspx/)
 		certNo = form.field_with(:name=>FieldNames['Cert Number'])	
 										
 		form.field_with(:name=>FieldNames['Cert Type']).options[1..-1].each do |var|
+			puts cnum
 			if(var.value == 'CNA')
 				form[FieldNames['Cert Type']]='CNA'
-				form[FieldNames['First Name']]='BENJAMIN P'
-				form[FieldNames['Last Name']]='CAACON'
-				form[FieldNames['Cert Number']]='00561824'
+				form[FieldNames['First Name']]=fname #'BENJAMIN P'
+				form[FieldNames['Last Name']]=lname #'CAACON'
+				form[FieldNames['Cert Number']]=cnum#'00561824'
 				form.radiobuttons[1].checked=true
 				#form.radiobuttons[1].checked=true
 				search_result = form.submit(form.button_with(:value=>'Go'))
 				l = agent.submit(form, form.buttons.first)
-				#puts l.body
-				doc=l.body
-				doc.search("div")
-				if l.body.include? /No results/
-					print "Nope"
+				final = l.body
+				puts cnum
+				puts fname
+				re = cnum
+				re1 = fname
+				re2 = lname
+				re3 = /(No data was found that matches your search criteria. Please try again.)/
+				if final.match(re3)
 					return false
 				else
-					puts "Exists"
+						if(final.match(re1)&&final.match(re2)&&final.match(re))
+						puts "Exists"
+						end
 					puts  FieldNames['Cert Number']
 					return true
 				end
-			end
 		end
 	end
 end
 
-url = 'http://www.apps.cdph.ca.gov/cvl/SearchPage.aspx'
-p = URLMaker.new(url)
-p.InitURL()
-
-
+end
